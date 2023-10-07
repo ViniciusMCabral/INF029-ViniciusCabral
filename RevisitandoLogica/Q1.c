15fa249a0d9dfae6f74ea595e46551516a307de9
ViniciusMCabral <144285203+ViniciusMCabral@users.noreply.github.com>
2023-10-07 18:18:49 +0000

    Questao1 Logica de Programacao

---
+++ RevisitandoLogica/Q1.c
@@ -0,0 +1,7 @@
+/* Faça um programa que imprima o seu nome, sua matrícula, e o seu semestre de ingresso no curso. Cada informação em uma linha. */
+#include <stdio.h>
+int main(){
+  printf("Nome: Vinicius Machak Cabral\n");
+  printf("Matricula: 20231160051\n");
+  printf("Semestre de ingresso: 2023.1\n");
+}
