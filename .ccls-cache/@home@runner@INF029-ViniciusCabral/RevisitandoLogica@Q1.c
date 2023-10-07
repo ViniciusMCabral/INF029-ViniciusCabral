2161aab4887949b02f99e5d2df3a7d494c960481
ViniciusMCabral <144285203+ViniciusMCabral@users.noreply.github.com>
2023-10-07 15:20:41 -0300

    Delete .ccls-cache/@home@runner@INF029-ViniciusCabral directory

--- .ccls-cache/@home@runner@INF029-ViniciusCabral/RevisitandoLogica@Q1.c
+++
@@ -1,7 +0,0 @@
-/* Faça um programa que imprima o seu nome, sua matrícula, e o seu semestre de ingresso no curso. Cada informação em uma linha. */
-#include <stdio.h>
-int main(){
-  printf("Nome: Vinicius Machak Cabral\n");
-  printf("Matricula: 20231160051\n");
-  printf("Semestre de ingresso: 2023.1\n");
-}
